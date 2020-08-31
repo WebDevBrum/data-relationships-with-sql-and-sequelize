ceb7cbe722d735d9e7bd98eb6d9b7f5da93a827f
Scott Davies <scottdavies1983@gmail.com>
2020-08-31 07:05:11 +0100

    end of course

---
+++ db/models/movie.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+
+module.exports = (sequelize) => {
+  class Movie extends Sequelize.Model {}
+  Movie.init(
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      releaseYear: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    },
+    { sequelize },
+  );
+
+  Movie.associate = (models) => {
+    // TODO Add associations.
+    Movie.belongsTo(models.Person, {
+      as: 'director', // alias
+      foreignKey: {
+        fieldName: 'directorPersonId',
+        allowNull: false,
+      },
+    });
+  };
+
+  return Movie;
+};
